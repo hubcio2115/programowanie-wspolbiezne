85a34b69a9e1e91ead289a9e2c6e244800e86974
hubcio2115 <kowalski.hubercik1@gmail.com>
2023-10-07 18:31:41 +0200

    rozwiązanie lab1

---
+++ lab1/server/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+const (
+	IN_FILE_PATH  = "./in.txt"
+	OUT_FILE_PATH = "./out.txt"
+)
+
+func doCalculations(num int) int {
+	return num*num + 3*num + 6
+}
+
+func main() {
+	for {
+		fmt.Println("Listening...")
+
+		inFile, inFileErr := os.OpenFile(IN_FILE_PATH, os.O_RDONLY, 0644)
+		if inFileErr != nil {
+			log.Panic("Error while opening/creating in.txt:", inFileErr)
+		}
+		defer inFile.Close()
+
+		outFile, outFileErr := os.OpenFile(OUT_FILE_PATH, os.O_TRUNC|os.O_WRONLY, 0644)
+		if outFileErr != nil {
+			log.Fatal("Error while opening/creating in.txt:", inFileErr)
+		}
+
+		var inputValue int
+		scanner := bufio.NewScanner(inFile)
+		if scanner.Scan() {
+			line := scanner.Text()
+
+			parsedLine, err := strconv.Atoi(line)
+			if err != nil {
+				outFile.WriteString("Error while parsing the input. It probably cannot be parsed into an integer.")
+
+				inFile.Close()
+				outFile.Close()
+				continue
+			}
+
+			inputValue = doCalculations(parsedLine)
+		}
+
+		outFile.WriteString(fmt.Sprintf("%d", inputValue))
+
+		inFile.Close()
+		outFile.Close()
+
+		time.Sleep(time.Second / 2)
+	}
+}
